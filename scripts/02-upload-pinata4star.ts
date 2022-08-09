410cf1648d5ee1a16184ed6ad9e2cb2f6803d3e7
CRYS <mariano.christopher123@gmail.com>
2022-08-09 21:48:01 +0800

    edited pinata upload scripts

---
+++ scripts/02-upload-pinata4star.ts
@@ -0,0 +1,56 @@
+import { storeImages, storeTokenUriMetadata } from "../utils/uploadToPinata"
+
+const imagesLocation = "./images/star4"
+const metadataTemplate = {
+    name: "",
+    description: "",
+    image: "",
+    attributes: [
+        {
+            max_hp: 900,
+            atk: 100,
+            def: 90,
+            elemental_mastery: 0,
+            max_stamina: 224,
+        },
+    ],
+}
+
+let tokenUris = []
+
+const pinataUpload4Stars = async () => {
+    //get the IPFS hashes ofour images(Methods below)
+    //1. With our IPFS node. https://docs.ipfs.io/
+    //2. pinata https://www.pinata.cloud/   pinata-node-sdk
+    //3. nft.storage(uses filecoin network) https://nft.storage/
+    if (process.env.UPLOAD_TO_PINATA == "true") {
+        tokenUris = await handleTokenUris()
+    }
+}
+
+// await storeImages(imagesLocation)
+
+const handleTokenUris = async () => {
+    // Check out https://github.com/PatrickAlphaC/nft-mix for a pythonic version of uploading
+    // to the raw IPFS-daemon from https://docs.ipfs.io/how-to/command-line-quick-start/
+    // You could also look at pinata https://www.pinata.cloud/
+    tokenUris = []
+    //store the image in IPFS
+    //store the metadata in IPFS
+    const { responses: imageUploadResponses, files } = await storeImages(imagesLocation)
+    for (const imageUploadResponseIndex in imageUploadResponses) {
+        //create metadata
+        //upload the metadata
+        let tokenUriMetadata = { ...metadataTemplate } //... unpack
+        tokenUriMetadata.name = files[imageUploadResponseIndex].replace(".png", "")
+        tokenUriMetadata.description = `An adorable ${tokenUriMetadata.name} pup!`
+        tokenUriMetadata.image = `ipfs://${imageUploadResponses[imageUploadResponseIndex].IpfsHash}`
+        console.log(`Uploading ${tokenUriMetadata.name}...`)
+        // store the JSON to pinata/IPFS
+        const metadataUploadResponse = await storeTokenUriMetadata(tokenUriMetadata)
+        tokenUris.push(`ipfs://${metadataUploadResponse!.IpfsHash}`)
+    }
+    console.log("Token URIs Uploaded! They are:")
+    console.log(tokenUris)
+    return tokenUris
+}
